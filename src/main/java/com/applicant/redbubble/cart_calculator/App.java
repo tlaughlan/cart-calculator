e834f6772439a048410a483ad5f0e39d6357adb6
Redbubble Applicant <me@example.com>
2019-12-30 17:25:56 +1100

    Configure pom for jar packaging

---
+++ src/main/java/com/applicant/redbubble/cart_calculator/App.java
@@ -0,0 +1,7 @@
+package com.applicant.redbubble.cart_calculator;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Here is my cart calculator!");
+    }
+}
