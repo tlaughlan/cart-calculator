1ac186c2965f6cb966ec5034562f784c7810cc9e
Redbubble Applicant <me@example.com>
2020-01-02 22:39:00 +1100

    Get Jackson working

--- src/main/java/com/applicant/redbubble/cart_calculator/App.java
+++ src/main/java/com/applicant/redbubble/cart_calculator/App.java
@@ -1,7 +1,26 @@
 package com.applicant.redbubble.cart_calculator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Here is my cart calculator!");
+        if (args.length != 2) {
+            System.out.println("Please specify two arguments: a cart json file, and a base prices json file.");
+            return;
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            File cartFile = new File(args[0]);
+            Object cartIn = objectMapper.readValue(cartFile, Object.class);
+            System.out.println(cartIn);
+        } catch (IOException ioe) {
+            System.out.print("Trouble reading cart file >>\n" + ioe.getMessage());
+            return;
+        }
     }
+
 }
