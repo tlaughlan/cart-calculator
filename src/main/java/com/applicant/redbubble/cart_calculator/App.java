18f274792c40dc29e6042c1ccf40763b36ebb2fa
Redbubble Applicant <me@example.com>
2020-01-04 01:26:10 +1100

    Implement log4j2 for logging

--- src/main/java/com/applicant/redbubble/cart_calculator/App.java
+++ src/main/java/com/applicant/redbubble/cart_calculator/App.java
@@ -3,12 +3,17 @@ package com.applicant.redbubble.cart_calculator;
 import com.applicant.redbubble.cart_calculator.models.Product;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 public class App {
+
+    static final Logger logger = LogManager.getLogger(App.class.getName());
+
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Please specify two arguments: a cart json file, and a base prices json file.");
@@ -20,9 +25,8 @@ public class App {
             File cartFile = new File(args[0]);
             List<Product> cart = objectMapper.readValue(cartFile, new TypeReference<List<Product>>(){});
         } catch (IOException ioe) {
-            System.out.print("Trouble reading cart file >>\n" + ioe.getMessage());
+            logger.error("Trouble reading file >> " + ioe.getMessage());
             return;
         }
     }
-
 }
