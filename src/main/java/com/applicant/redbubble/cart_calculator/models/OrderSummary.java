c897723f33ece5166b27f144fc8e6b614c83c5eb
tlaughlan <twlaughlan@gmail.com>
2020-12-03 18:46:58 +1100

    Remove unnecessary logger

--- src/main/java/com/applicant/redbubble/cart_calculator/models/OrderSummary.java
+++ src/main/java/com/applicant/redbubble/cart_calculator/models/OrderSummary.java
@@ -1,15 +1,10 @@
 package com.applicant.redbubble.cart_calculator.models;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class OrderSummary {
-
-    static final Logger logger = LogManager.getLogger(OrderSummary.class.getName());
 
     private Integer orderTotal;
 
